654d4fbe7031cfe5055ed984210eab01fc19b078
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-15 16:51:42 -0500

    data fix for tools are clutch

--- Server/DataBase/insert_data.js
+++ Server/DataBase/insert_data.js
@@ -3,21 +3,25 @@ const fs = require('fs');
 const Galaxy = require('./schema'); // Import the schema
 
 // MongoDB connection URI
-const mongoDBAtlasUri = 'mongodb://localhost:27017';
+const mongoDBAtlasUri = 'mongodb+srv://<username>:<password>@<cluster>/<database>?retryWrites=true&w=majority';
 
 // Connect to MongoDB database
 mongoose.connect(mongoDBAtlasUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Read the JSON file containing data to insert
-const jsonData = fs.readFileSync('planet_data.json', 'utf8');
-const galaxiesData = JSON.parse(jsonData);
+try {
+    const jsonData = fs.readFileSync('planet_data.json', 'utf8');
+    const galaxiesData = JSON.parse(jsonData);
 
-// Insert data into MongoDB using the schema
-Galaxy.insertMany(galaxiesData)
-  .then(() => {
-    console.log('Data inserted successfully');
-    mongoose.connection.close(); // Close the connection after inserting data
-  })
-  .catch((error) => {
-    console.error('Error inserting data:', error);
-  });
+    // Insert data into MongoDB using the schema
+    Galaxy.insertMany(galaxiesData)
+        .then(() => {
+            console.log('Data inserted successfully');
+            // Consider whether to close the connection here
+        })
+        .catch((error) => {
+            console.error('Error inserting data:', error);
+        });
+} catch (error) {
+    console.error('Error reading file:', error);
+}
