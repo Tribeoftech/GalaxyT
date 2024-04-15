3175756b19dbdaab7fa524d7ed66952a3d6225e0
Tribeoftech <gratefulgrowing333@gmail.com>
2024-04-15 14:46:42 -0500

    app

--- Server/DataBase/app.js
+++ Server/DataBase/app.js
@@ -9,7 +9,7 @@ mongoose.connection.once('open', () => {
     console.log('connected to database');
 });
 
-port = 2500;
+port = 8000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
   });
